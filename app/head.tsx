5f287d870f512c3d0c0aecd0ebf4895e011f8203
Anthony Fernandez <anthonyfdez23@gmail.com>
2023-01-09 18:41:37 -0500

    Started adding eslint for a bulletproof project

--- app/head.tsx
+++ app/head.tsx
@@ -1,9 +1,11 @@
-export default function Head() {
+import React from "react";
+
+export default function Head(): JSX.Element {
   return (
     <>
-      <title></title>
-      <meta content='width=device-width, initial-scale=1' name='viewport' />
-      <link rel='icon' href='/favicon.ico' />
+      <title>App</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link href="/favicon.ico" rel="icon" />
     </>
-  )
+  );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,22 @@
-import { Heebo } from '@next/font/google'
-import styles from './app.css'
-import NavBar from './nav-bar'
-const heebo = Heebo({ subsets: ['latin'] })
+import React from "react";
+import { Heebo } from "@next/font/google";
+import styles from "./app.css";
+// import NavBar from "./nav-bar";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+const heebo = Heebo({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}): JSX.Element {
   return (
-    <html lang='en' className={`${heebo.className} ${styles.enable}`}>
+    <html lang="en" className={`${heebo.className} ${styles.enable}`}>
       <head />
-      <body style={{ padding: '0 5rem', backgroundColor: '#131722' }}>
-        <NavBar></NavBar>
+      <body style={{ padding: "0 5rem", backgroundColor: "#131722" }}>
+        {/* <NavBar></NavBar> */}
         <div>{children}</div>
       </body>
     </html>
-  )
+  );
 }

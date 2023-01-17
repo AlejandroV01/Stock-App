09a43b28cc406bcf210224d6826b5775340843b1
Alejandro <alexvera0109@gmail.com>
2023-01-17 18:25:55 -0500

    init tanstack react table

---
+++ components/HomeReactTable/HomeReactTable.tsx
@@ -0,0 +1,12 @@
+import { useReactTable } from "@tanstack/react-table";
+import React from "react";
+
+const HomeReactTable = () => {
+  return (
+    <div>
+      <h1>HomeReactTable</h1>
+    </div>
+  );
+};
+
+export default HomeReactTable;
